3a3539ee23744d94bfefbb66f06e547ae2f847f1
hiramfleitas <hiramfleitas@hotmail.com>
2019-08-09 19:49:29 -0400

    fixed adf-dw

--- ASDW/adf-dw.sql
+++ ASDW/adf-dw.sql
@@ -1,23 +1,4 @@
-drop table character;
-go
-create table [Character] ( 
-	CharacterId		int             	not null,
-    FullName		varchar(50)			not null,
-	Affiliation		varchar(60)			null, 
-	Category		varchar(10)			null, 
-	Aka				varchar(300)		null, --*
-	[Status]		varchar(35)			null,
-	Race			varchar(50)			null,
-	Age				int					null,
-	Home			varchar(50)			null,
-	Relatives		varchar(300)		null, --*
-	Weapons			varchar(100)		null, --*
-	EyeColor		varchar(20)			null,
-	HairColor		varchar(50)			null,
-	Minions			varchar(100)		null,
-	VoicedBy		varchar(50)			null,
-);
-go
+--Leads
 
 if object_id('Customer') is not null drop table Customer
 begin
@@ -25,15 +6,24 @@ begin
  		 [CustomerId]	int 
 		,[Name]			nvarchar(256)
 		,[Email]		varchar(320)
+	)
+	with (
+		distribution = hash(CustomerId),
+		clustered columnstore index
 	);
 end
 go
+
 if object_id('CustomerAccount') is not null drop table CustomerAccount
 begin
 	create table CustomerAccount (
  		 [CustomerAccountId]	int 
 		,[CustomerId]			int 
 		,[AcctNumEnding]		int
+	)
+	with (
+		distribution = hash(CustomerId),
+		clustered columnstore index
 	);
 end
 go
@@ -67,3 +57,49 @@ begin
 	);
 end
 go
+
+--Trollhunters
+if object_id('Character') is not null drop table Character
+go
+create table [Character] ( 
+	CharacterId		int             	not null,
+    FullName		varchar(50)			not null,
+	Affiliation		varchar(60)			null, 
+	Category		varchar(10)			null, 
+	Aka				varchar(300)		null, --*
+	[Status]		varchar(35)			null,
+	Race			varchar(50)			null,
+	Age				int					null,
+	Home			varchar(50)			null,
+	Relatives		varchar(300)		null, --*
+	Weapons			varchar(100)		null, --*
+	EyeColor		varchar(20)			null,
+	HairColor		varchar(50)			null,
+	Minions			varchar(100)		null,
+	VoicedBy		varchar(50)			null
+) 
+with (
+	distribution = hash(CharacterId),
+	clustered columnstore index
+);
+go
+
+if object_id('quotes') is not null drop table quotes
+create table quotes (
+    quoteid     int             ,
+    character   varchar(128)    not null,
+    quote       nvarchar(4000)   not null,
+    sentiment   float
+);
+go 
+
+select * from dbo.Character
+select * from dbo.Quotes
+
+select * from dbo.Customer
+select * from dbo.CustomerAccount
+
+Res: 
+-- https://docs.microsoft.com/en-us/azure/data-factory/copy-activity-performance
+-- https://docs.microsoft.com/en-us/azure/data-factory/connector-azure-sql-data-warehouse
+-- https://docs.microsoft.com/en-us/azure/data-factory/connector-sql-server#append-data
