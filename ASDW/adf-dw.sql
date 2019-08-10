9c0a9966bb1a6ca27be8a4ae58fe2973f3e99cb0
hiramfleitas <hiramfleitas@hotmail.com>
2019-08-10 13:54:40 -0400

    fixed adf-dw

--- ASDW/adf-dw.sql
+++ ASDW/adf-dw.sql
@@ -1,87 +1,92 @@
---Leads
+if object_id('wipe') is not null drop proc wipe;
+go 
+create proc dbo.wipe
+as 
+	-- + ----- +
+	-- | Leads |
+	-- + ----- +
+	if object_id('Customer') is not null drop table Customer
+	create table Customer (
+			[CustomerId]	int 
+		,[Name]			nvarchar(256)
+		,[Email]		varchar(320)
+	)
+	with (
+		distribution = hash(CustomerId),
+		clustered columnstore index
+	);
 
-if object_id('Customer') is not null drop table Customer
-create table Customer (
-		[CustomerId]	int 
-	,[Name]			nvarchar(256)
-	,[Email]		varchar(320)
-)
-with (
-	distribution = hash(CustomerId),
-	clustered columnstore index
-);
+	if object_id('CustomerAccount') is not null drop table CustomerAccount
+	create table CustomerAccount (
+			[CustomerAccountId]	int 
+		,[CustomerId]			int 
+		,[AcctNumEnding]		int
+	)
+	with (
+		distribution = hash(CustomerId),
+		clustered columnstore index
+	);
 
-if object_id('CustomerAccount') is not null drop table CustomerAccount
-create table CustomerAccount (
-		[CustomerAccountId]	int 
-	,[CustomerId]			int 
-	,[AcctNumEnding]		int
-)
-with (
-	distribution = hash(CustomerId),
-	clustered columnstore index
-);
+	if object_id('Lead') is not null drop table Lead
+	create table Lead (
+			[LeadId]		int 
+		,[Name]			nvarchar(256)
+		,[Email]		varchar(320)
+		,[Address]		nvarchar(256)
+		,[Status]		bit 
+		,[CreatedBy]	nvarchar(128) 
+		,[CreatedOn]	datetime 
+		,[ModifiedBy]	nvarchar(128) 
+		,[ModifiedOn]	datetime 
+	);
 
-if object_id('Lead') is not null drop table Lead
-create table Lead (
-		[LeadId]		int 
-	,[Name]			nvarchar(256)
-	,[Email]		varchar(320)
-	,[Address]		nvarchar(256)
-	,[Status]		bit 
-	,[CreatedBy]	nvarchar(128) 
-	,[CreatedOn]	datetime 
-	,[ModifiedBy]	nvarchar(128) 
-	,[ModifiedOn]	datetime 
-);
+	if object_id('Ticket') is not null drop table Ticket
+	create table Ticket (
+			[TicketId]			int 
+		,[CustomerId]		int 
+		,[AcctNumEnding]	int	
+		,[Status]			bit 
+		,[LeadId]			int 
+		,[CreatedBy]		nvarchar(128) 
+		,[CreatedOn]		datetime 
+		,[ModifiedBy]		nvarchar(128) 
+		,[ModifiedOn]		datetime
+	);
 
-if object_id('Ticket') is not null drop table Ticket
-create table Ticket (
-		[TicketId]			int 
-	,[CustomerId]		int 
-	,[AcctNumEnding]	int	
-	,[Status]			bit 
-	,[LeadId]			int 
-	,[CreatedBy]		nvarchar(128) 
-	,[CreatedOn]		datetime 
-	,[ModifiedBy]		nvarchar(128) 
-	,[ModifiedOn]		datetime
-);
+	-- + ------------ +
+	-- | Trollhunters |
+	-- + ------------ +
+	if object_id('Character') is not null drop table Character
+	create table [Character] ( 
+		CharacterId		int             	not null,
+		FullName		varchar(50)			not null,
+		Affiliation		varchar(60)			null, 
+		Category		varchar(10)			null, 
+		Aka				varchar(300)		null, --*
+		[Status]		varchar(35)			null,
+		Race			varchar(50)			null,
+		Age				int					null,
+		Home			varchar(50)			null,
+		Relatives		varchar(300)		null, --*
+		Weapons			varchar(100)		null, --*
+		EyeColor		varchar(20)			null,
+		HairColor		varchar(50)			null,
+		Minions			varchar(100)		null,
+		VoicedBy		varchar(50)			null
+	) 
+	with (
+		distribution = hash(CharacterId),
+		clustered columnstore index
+	);
 
-
---Trollhunters
-if object_id('Character') is not null drop table Character
+	if object_id('quotes') is not null drop table quotes
+	create table quotes (
+		quoteid     int             ,
+		character   varchar(128)    not null,
+		quote       nvarchar(4000)   not null,
+		sentiment   float
+	);
 go
-create table [Character] ( 
-	CharacterId		int             	not null,
-    FullName		varchar(50)			not null,
-	Affiliation		varchar(60)			null, 
-	Category		varchar(10)			null, 
-	Aka				varchar(300)		null, --*
-	[Status]		varchar(35)			null,
-	Race			varchar(50)			null,
-	Age				int					null,
-	Home			varchar(50)			null,
-	Relatives		varchar(300)		null, --*
-	Weapons			varchar(100)		null, --*
-	EyeColor		varchar(20)			null,
-	HairColor		varchar(50)			null,
-	Minions			varchar(100)		null,
-	VoicedBy		varchar(50)			null
-) 
-with (
-	distribution = hash(CharacterId),
-	clustered columnstore index
-);
-
-if object_id('quotes') is not null drop table quotes
-create table quotes (
-    quoteid     int             ,
-    character   varchar(128)    not null,
-    quote       nvarchar(4000)   not null,
-    sentiment   float
-);
-
 
 select * from dbo.Character
 select * from dbo.Quotes
--- ASDW/adf-hs.sql
+++ ASDW/adf-hs.sql
@@ -1,88 +1,87 @@
---Leads
 
-drop table if exists Ticket
-drop table if exists Lead
-drop table if exists CustomerAccount
-drop table if exists Customer
+drop proc if exists wipe;
 go
+create or alter proc dbo.wipe
+as 
+	-- + ----- +
+	-- | Leads |
+	-- + ----- +
+    drop table if exists Ticket;
+    drop table if exists Lead;
+    drop table if exists CustomerAccount;
+    drop table if exists Customer;
 
-drop table if exists Customer;
-create table Customer (
+    create table Customer (
         [CustomerId]	int identity(1,1)
-    ,[Name]			nvarchar(256)
-    ,[Email]		varchar(320)
-    constraint pkCustomer primary key clustered ([CustomerId] asc)
-);
-go
+        ,[Name]			nvarchar(256)
+        ,[Email]		varchar(320)
+        constraint pkCustomer primary key clustered ([CustomerId] asc)
+    );
 
-drop table if exists CustomerAccount;
-create table CustomerAccount (
+    create table CustomerAccount (
         [CustomerAccountId]	int --identity(1,1)
-    ,[CustomerId]			int --foreign key references Customer(CustomerId)
-    ,[AcctNumEnding]		int
-    constraint pkCustomerAccount primary key clustered ([CustomerAccountId] asc)
-);
-go
+        ,[CustomerId]			int --foreign key references Customer(CustomerId)
+        ,[AcctNumEnding]		int
+        constraint pkCustomerAccount primary key clustered ([CustomerAccountId] asc)
+    );
 
-drop table if exists Lead;
-create table Lead (
-    [LeadId]		int --identity(1001,1)
-    ,[Name]			nvarchar(256)
-    ,[Email]		varchar(320)
-    ,[Address]		nvarchar(256)
-    ,[Status]		bit default(0) -- 0 open / 1 closed
-    ,[CreatedBy]	nvarchar(128) default suser_name()
-    ,[CreatedOn]	datetime default getdate()
-    ,[ModifiedBy]	nvarchar(128) 
-    ,[ModifiedOn]	datetime 
-    constraint pkLeads primary key clustered ([LeadId] asc)
-);
-go
+    create table Lead (
+        [LeadId]		int --identity(1001,1)
+        ,[Name]			nvarchar(256)
+        ,[Email]		varchar(320)
+        ,[Address]		nvarchar(256)
+        ,[Status]		bit default(0) -- 0 open / 1 closed
+        ,[CreatedBy]	nvarchar(128) default suser_name()
+        ,[CreatedOn]	datetime default getdate()
+        ,[ModifiedBy]	nvarchar(128) 
+        ,[ModifiedOn]	datetime 
+        constraint pkLeads primary key clustered ([LeadId] asc)
+    );
 
-drop table if exists Ticket;
-create table Ticket (
-        [TicketId]			int --identity(2001,1)
-    ,[CustomerId]		int 
-    ,[AcctNumEnding]	int	
-    ,[Status]			bit default(0) -- 0 open / 1 closed
-    ,[LeadId]			int foreign key references Lead(LeadId)
-    ,[CreatedBy]		nvarchar(128) default suser_name()
-    ,[CreatedOn]		datetime default getdate()
-    ,[ModifiedBy]		nvarchar(128) default suser_name()
-    ,[ModifiedOn]		datetime default getdate()
-    constraint pkTicket primary key clustered ([TicketId] asc)
-);
-go
+    create table Ticket (
+            [TicketId]			int --identity(2001,1)
+        ,[CustomerId]		int 
+        ,[AcctNumEnding]	int	
+        ,[Status]			bit default(0) -- 0 open / 1 closed
+        ,[LeadId]			int foreign key references Lead(LeadId)
+        ,[CreatedBy]		nvarchar(128) default suser_name()
+        ,[CreatedOn]		datetime default getdate()
+        ,[ModifiedBy]		nvarchar(128) default suser_name()
+        ,[ModifiedOn]		datetime default getdate()
+        constraint pkTicket primary key clustered ([TicketId] asc)
+    );
 
 
---Trollhunters
-drop table if exists [Character];
-create table [Character] ( 
-	CharacterId		int identity(1,1)	not null,
-    FullName		varchar(50)			not null,
-	Affiliation		varchar(60)			null, 
-	Category		varchar(10)			null, 
-	Aka				varchar(300)		null, --*
-	[Status]		varchar(35)			null,
-	Race			varchar(50)			null,
-	Age				int					null,
-	Home			varchar(50)			null,
-	Relatives		varchar(300)		null, --*
-	Weapons			varchar(100)		null, --*
-	EyeColor		varchar(20)			null,
-	HairColor		varchar(50)			null,
-	Minions			varchar(100)		null,
-	VoicedBy		varchar(50)			null,
-	constraint pk_Character primary key clustered (CharacterId asc));
-go
+	-- + ------------ +
+	-- | Trollhunters |
+	-- + ------------ +
+    drop table if exists [Character];
+    drop table if exists quotes;
 
-drop table if exists quotes;
-create table quotes (
-    quoteid     int             identity(1,1) primary key clustered,
-    character   varchar(128)    not null,
-    quote       nvarchar(max)   not null,
-    sentiment   float
-);
+    create table [Character] ( 
+        CharacterId		int identity(1,1)	not null,
+        FullName		varchar(50)			not null,
+        Affiliation		varchar(60)			null, 
+        Category		varchar(10)			null, 
+        Aka				varchar(300)		null, --*
+        [Status]		varchar(35)			null,
+        Race			varchar(50)			null,
+        Age				int					null,
+        Home			varchar(50)			null,
+        Relatives		varchar(300)		null, --*
+        Weapons			varchar(100)		null, --*
+        EyeColor		varchar(20)			null,
+        HairColor		varchar(50)			null,
+        Minions			varchar(100)		null,
+        VoicedBy		varchar(50)			null,
+        constraint pk_Character primary key clustered (CharacterId asc));
+
+    create table quotes (
+        quoteid     int             identity(1,1) primary key clustered,
+        character   varchar(128)    not null,
+        quote       nvarchar(max)   not null,
+        sentiment   float
+    );
 go 
 
 select * from dbo.Character
