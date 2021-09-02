19cf3fd68dbab6e3a65a5b55f3a6faa5aac7d4fa
Hiram Fleitas <hiramfleitas@hotmail.com>
2021-09-02 17:14:48 -0400

    Add files via upload

---
+++ ASDW/4AddUser-LoaderXL.sql
@@ -0,0 +1,23 @@
+--dw
+LoaderXL
+
+if exists (select 1 from sys.sql_logins where name='LoaderXL') drop login LoaderXL;
+create login LoaderXL with password = 'XL!dw2019'
+alter login LoaderXL enable
+go 
+drop user LoaderXL;
+go
+create user LoaderXL for login LoaderXL
+alter role dbmanager add member LoaderXL
+
+--sql-eastus2-int-dev-reboot-analytics_wh
+drop user LoaderXL;
+create user LoaderXL for login LoaderXL
+go
+exec sp_addrolemember db_owner, LoaderXL;
+EXEC sp_addrolemember 'xlargerc', 'LoaderXL'
+
+grant view definition to public;
+grant showplan to LoaderXL;
+grant connect to LoaderXL
+go 
